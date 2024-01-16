63cb4b717e784b33919523329637c281c1717e79
Divyendu Narayan <divyendu.narayan@gmail.com>
2024-01-16 01:08:06 -0800

    start and stop date time

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 import os
 vin = os.getenv("VIN")
+start_time = os.getenv("Start Date Time")
+stop_time = os.getenv("Stop Date Time")
 print('Hello World')
 print("VIN = {}".format(vin))
+print("Start Date Time = {}".format(start_time))
+print("Stop Date Time = {}".format(stop_time))
